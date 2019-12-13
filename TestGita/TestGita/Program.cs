6114a1a58774b3f2e3f4f723a2969fbf9b9ee310
tijanaENG <tijana.ilic@eng.it>
2019-12-13 14:59:38 +0100

    Second change of message

--- TestGita/TestGita/Program.cs
+++ TestGita/TestGita/Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
-
-namespace TestGita
+﻿namespace TestGita
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Hello Tijana!");
+            Console.WriteLine("Hello World! Hello Tijana!Hello Tijana2");
+           
             
         }
     }
